125a453e3580e8330b97e56cf87e5a2ca787b044
Denizhan Bakıryor <denizhanbakiryor@gmail.com>
2023-10-02 17:46:56 +0300

    S10G3 projesi çözüldü.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,96 @@
-import { useState } from "react";
-import { Switch, Route, NavLink } from "react-router-dom";
-import Movie from "./components/Movie";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, Route, Switch } from "react-router-dom";
+import {
+  addFavorite,
+  firstMovie,
+  nextMovie,
+  prevMovie,
+} from "./Actions/actions";
 import FavMovie from "./components/FavMovie";
+import Movie from "./components/Movie";
 
 function App() {
-  const [sira, setSira] = useState(0);
-  const favMovies = [];
+  const sira = useSelector((store) => store.sira);
+  const favMovies = useSelector((store) => store.favMovies);
 
-  function sonrakiFilm() {
-    setSira(sira + 1);
+  const movies = useSelector((store) => store.movies);
+
+  const dispatch = useDispatch();
+
+  function handleNext() {
+    dispatch(nextMovie());
+  }
+
+  function handlePrev() {
+    dispatch(prevMovie());
+  }
+
+  function handleFirstMovie() {
+    dispatch(firstMovie());
+  }
+
+  function handleAddFavorite() {
+    dispatch(addFavorite(movies[sira]));
   }
 
   return (
     <div className="wrapper max-w-2xl mx-auto">
       <nav className="flex text-2xl pb-6 pt-8 gap-2 justify-center">
-        <NavLink to="/" exact className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/"
+          exact
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Filmler
         </NavLink>
-        <NavLink to="/listem" className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/listem"
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Listem
         </NavLink>
       </nav>
       <Switch>
         <Route exact path="/">
-          <Movie sira={sira} />
+          {movies.length > 0 && <Movie sira={sira} />}
 
           <div className="flex gap-3 justify-end py-3">
-            <button
-              onClick={sonrakiFilm}
-              className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
-            >
-              Sıradaki
-            </button>
-            <button className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white">
-              Listeme ekle
-            </button>
+            {sira > 0 && (
+              <>
+                <button
+                  onClick={handleFirstMovie}
+                  className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+                >
+                  Başa Dön
+                </button>
+                <button
+                  onClick={handlePrev}
+                  className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+                >
+                  Önceki
+                </button>
+              </>
+            )}
+
+            {movies.length > 0 && (
+              <>
+                <button
+                  disabled={movies.length - 1 === sira}
+                  onClick={handleNext}
+                  className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+                >
+                  Sıradaki
+                </button>
+                <button
+                  onClick={handleAddFavorite}
+                  className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white"
+                >
+                  Listeme ekle
+                </button>
+              </>
+            )}
           </div>
         </Route>
 
---
+++ src/Reducers/movieReducer.js
@@ -0,0 +1,60 @@
+import {
+  ADD_FAVORITE,
+  FIRST_MOVIE,
+  NEXT_MOVIE,
+  PREV_MOVIE,
+  REMOVE_FAVORITE,
+} from "../Actions/actions";
+import { movies } from "../movies";
+const initialState = {
+  sira: 0,
+  movies: movies,
+  favMovies: [],
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case NEXT_MOVIE:
+      return {
+        ...state,
+        sira: state.sira + 1,
+      };
+    case PREV_MOVIE:
+      return {
+        ...state,
+        sira: state.sira - 1,
+      };
+    case FIRST_MOVIE:
+      return {
+        ...state,
+        sira: 0,
+      };
+    case ADD_FAVORITE:
+      const oldMovie = state.favMovies.find(
+        (movie) => movie.id === action.payload.id
+      );
+      if (oldMovie) {
+        return state;
+      } else {
+        return {
+          ...state,
+          favMovies: [...state.favMovies, action.payload],
+          movies: state.movies.filter(
+            (movie) => movie.id !== action.payload.id
+          ),
+        };
+      }
+
+    case REMOVE_FAVORITE:
+      return {
+        ...state,
+        favMovies: state.favMovies.filter(
+          (movie) => movie.id !== action.payload
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
